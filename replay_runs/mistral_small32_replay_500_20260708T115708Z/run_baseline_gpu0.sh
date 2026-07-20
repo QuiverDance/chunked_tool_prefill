941ba4318b90bf8d9149f2c0d97ce901dbb6c222
QuiverDance <pjw7200@ajou.ac.kr>
2026-07-20 09:53:53 +0000

    data: preserve 128-token chunked prefill replay

---
+++ replay_runs/mistral_small32_replay_500_20260708T115708Z/run_baseline_gpu0.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+set -eu
+
+cd /home/pjw7200/chunked_tool_prefill/agent
+
+PYTHONPATH=src /home/pjw7200/chunked_tool_prefill/.conda/miniswe-py311/bin/python -m minisweagent.run.replay \
+  /home/pjw7200/chunked_tool_prefill/traces/swebench_verified_qwen36_trace_token_timing_full_20260706T113200Z \
+  -o /home/pjw7200/chunked_tool_prefill/replay_runs/mistral_small32_replay_500_20260708T115708Z/baseline_gpu0 \
+  --algorithm baseline \
+  --limit 500 \
+  -c swebench_replay_output_first \
+  -c /home/pjw7200/chunked_tool_prefill/replay_runs/mistral_small32_replay_500_20260708T115708Z/mistral_gpu0.yaml
